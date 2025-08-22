39ff892a1dab9c6c65b3788fbf8b1ea3aece2e00
Di3go0-0 <d.rincon@utp.edu.co>
2025-08-22 15:34:52 -0500

    Feaute: Update script

--- matrix/script.sh
+++ matrix/script.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
+echo "MatrixSize,ExecutionTime" >execution_times.csv
 for j in {1..10}; do
   for i in 10 100 200 400 600 800 1000 2000; do
+    START=$(date +%s.%N)
     ./matrix $i
+    END=$(date +%s.%N)
+    EXECUTION_TIME=$(echo "$END - $START" | bc)
+    echo "$i,$EXECUTION_TIME" >>execution_times.csv
   done
 done
